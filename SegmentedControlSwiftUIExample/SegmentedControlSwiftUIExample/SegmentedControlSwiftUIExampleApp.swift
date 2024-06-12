15d43b884ccb06a4a45bc6ef41a7fbd08456bca1
parveen-khan-ovyo <parveen.khan@ovyo.com>
2024-06-12 14:11:00 +0530

    Added swiftUI view and ViewController view example
    Added swiftUI view and ViewController view example

---
+++ SegmentedControlSwiftUIExample/SegmentedControlSwiftUIExample/SegmentedControlSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControlSwiftUIExampleApp.swift
+//  SegmentedControlSwiftUIExample
+//
+//  Created by Parveen Khan on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControlSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
